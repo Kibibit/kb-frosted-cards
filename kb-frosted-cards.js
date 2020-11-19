82672496ce036ac27d710dc14df1b923c2134a34
Neil Kalman <Neilkalman@gmail.com>
2020-11-19 05:53:21 +0200

    feat(injection): try and bulletproof the style injection

--- kb-frosted-cards.js
+++ kb-frosted-cards.js
@@ -1,59 +1,87 @@
-const cardMods = new Map();
-cardMods.set('ha-card', ':host { backdrop-filter: blur(5px) }');
-cardMods.set('ha-dialog', '.mdc-dialog__surface { backdrop-filter: blur(5px); }');
+(() => {
+  let waitedFor = 0;
+  const cardMods = new Map();
+  cardMods.set("ha-card", ":host { backdrop-filter: blur(5px) }");
+  cardMods.set(
+    "ha-dialog",
+    ".mdc-dialog__surface { backdrop-filter: blur(5px); }"
+  );
 
-const injectPromises = Array.from( cardMods ).map(([cardName, cssRule]) => addCssToCard(cardName, cssRule));
+  const injectPromises = Array.from(cardMods).map(([cardName, cssRule]) =>
+    addCssToCard(cardName, cssRule)
+  );
 
-Promise.resolve()
-.then(() => wait(5000))
-.then(() => Promise.all(injectPromises))
-.then(() => {
-  // Force lovelace to redraw everything
-  const ev = new Event("ll-rebuild", {
-    bubbles: true,
-    cancelable: false,
-    composed: true,
-  });
+  Promise.resolve()
+    .then(() => Promise.all(injectPromises))
+    .then(() => {
+      // Force lovelace to redraw everything
+      const ev = new Event("ll-rebuild", {
+        bubbles: true,
+        cancelable: false,
+        composed: true,
+      });
 
-  let root = document.querySelector("home-assistant");
-  root = root && root.shadowRoot;
-  root = root && root.querySelector("home-assistant-main");
-  root = root && root.shadowRoot;
-  root =
-    root && root.querySelector("app-drawer-layout partial-panel-resolver");
-  root = (root && root.shadowRoot) || root;
-  root = root && root.querySelector("ha-panel-lovelace");
-  root = root && root.shadowRoot;
-  root = root && root.querySelector("hui-root");
-  root = root && root.shadowRoot;
-  root = root && root.querySelector("ha-app-layout #view");
-  root = root && root.firstElementChild;
-  if (root) root.dispatchEvent(ev);
-});
+      let root = document.querySelector("home-assistant");
+      root = root && root.shadowRoot;
+      root = root && root.querySelector("home-assistant-main");
+      root = root && root.shadowRoot;
+      root =
+        root && root.querySelector("app-drawer-layout partial-panel-resolver");
+      root = (root && root.shadowRoot) || root;
+      root = root && root.querySelector("ha-panel-lovelace");
+      root = root && root.shadowRoot;
+      root = root && root.querySelector("hui-root");
+      root = root && root.shadowRoot;
+      root = root && root.querySelector("ha-app-layout #view");
+      root = root && root.firstElementChild;
+      if (root) root.dispatchEvent(ev);
+    });
 
-function wait(timeout) {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(), timeout || 3000);
-  });
-}
-
-function addCssToCard(cardName, cssRule) {
-  return Promise.resolve()
-  .then(() => customElements.whenDefined(cardName))
-  .then(() => customElements.get(cardName))
-  .then((cardClass) => insertStyleRule(cardClass, cssRule))
-  .catch((err) => console.error(err));
-}
-
-function insertStyleRule(card, rule) {
-  const newWay = Array.isArray(card.getStyles())
-    ? card.getStyles()[0].styleSheet
-    : card.getStyles().styleSheet;
-
-  const oldWay = card._styles && card._styles[0] && card._styles[0].styleSheet;
-
-  newWay.insertRule(rule);
-  if (oldWay && oldWay.insertRule) {
-    oldWay.insertRule(rule, 0);
+  function waitP(timeout) {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(), timeout || 3000);
+    });
   }
-}
+
+  function addCssToCard(cardName, cssRule) {
+    return Promise.resolve()
+      .then(() => waitUntilDefined(cardName))
+      .then((cardClass) => insertStyleRule(cardClass, cssRule))
+      .catch((err) => console.error(err));
+  }
+
+  function insertStyleRule(card, rule) {
+    const newWay = Array.isArray(card.getStyles())
+      ? card.getStyles()[0].styleSheet
+      : card.getStyles().styleSheet;
+
+    const oldWay =
+      card._styles && card._styles[0] && card._styles[0].styleSheet;
+
+    newWay.insertRule(rule);
+    if (oldWay && oldWay.insertRule) {
+      oldWay.insertRule(rule, 0);
+    }
+  }
+
+  function waitUntilDefined(elementName, timeout) {
+    timeout = timeout || 10000;
+    return Promise.resolve()
+    .then(() => customElements.get(elementName))
+    .then((customElement) => {
+      if (customElement && customElement._styles && customElement._styles[0]) {
+        waitedFor = 0;
+        return customElement;
+      }
+
+      if (waitedFor >= timeout) {
+        throw new Error(elementName + ' was not defined before timeout');
+      }
+
+      waitedFor += 2000;
+      return waitP(2000)
+        .then(() => waitUntilDefined(elementName));
+    })
+    
+  }
+})();
